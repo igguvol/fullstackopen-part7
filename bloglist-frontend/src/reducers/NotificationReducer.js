5644dd1eaa6948e7f1d78351acd295674b9ae5cb
igguvol <pelkoneno@gmail.com>
2018-11-15 22:21:45 +0200

    notificationReducer

---
+++ bloglist-frontend/src/reducers/NotificationReducer.js
@@ -0,0 +1,37 @@
+import { bindActionCreators } from 'redux'
+
+const initialState = {message:null}
+
+const reducer = (state = initialState, action) => {
+//  console.log('notificationReducer',action)
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return {...state,message:action.message,style:action.style}
+    case 'CLEAR_NOTIFICATION':
+      return {...state,message:'',style:''}
+    default:
+      return state
+  }
+}
+
+
+export const setNotification = (message,style='error') => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      message: message,
+      style:style
+
+    });
+    if ( message !== undefined && message !== null && message !== '' )
+    {
+      const k = setTimeout(() => {
+        dispatch({
+          type: 'CLEAR_NOTIFICATION',
+          });
+        }, 10000);
+    }
+  }
+}
+
+export default { reducer, setNotification };
